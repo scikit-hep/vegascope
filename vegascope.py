babbc9c4199a77e8cbf20ce222909311d2f40f55
Jim Pivarski <jpivarski@gmail.com>
2018-03-15 08:07:58 -0500

    trivial change to trigger Travis build

--- vegascope.py
+++ vegascope.py
@@ -50,7 +50,7 @@ else:
     from urllib.parse import urlparse
     unicode = str
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 version = __version__
 version_info = tuple(re.split(r"[-\.]", __version__))
 
